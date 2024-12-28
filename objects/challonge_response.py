5ac64afc8a724a305772e0e6011a4bb820d91d52
Das15 <62486730+Das15@users.noreply.github.com>
2024-12-28 03:07:21 +0100

    Refactored challonge score retrieval logic to avoid bugs caused by inconsistent CSV score formatting.

--- objects/challonge_response.py
+++ objects/challonge_response.py
@@ -1,4 +1,5 @@
 import logging
+import re
 
 import objects.bracket as bracket
 import objects.ladder as ladder
@@ -24,9 +25,9 @@ def assign_scores(match_data: match.Class, scores: []) -> match.Class:
     if scores[0] != "" and scores[1] != "":
         if scores[1] == "-1":
             match_data.Team1Score = match_data.PointsToWin
-            match_data.Team2Score = "0"
+            match_data.Team2Score = "-1"
         elif scores[0] == "-1":
-            match_data.Team1Score = "0"
+            match_data.Team1Score = "-1"
             match_data.Team2Score = match_data.PointsToWin
         else:
             match_data.Team1Score = scores[0]
@@ -47,6 +48,20 @@ def count_scores(scores: str) -> []:
     return returned_score
 
 
+def calculate_scores(scores: str) -> []:
+    parsing_regex = r"-{0,1}[0-9]{1,2}"
+    parsed_scores = re.findall(parsing_regex, scores)
+
+    # First case, score contains the collection of points, we need to count
+    if "," in scores:
+        return count_scores(scores)
+    # Second case, score string contains the final value, we dont need to count
+    # If the second score is not WBD, strip leading minus
+    if parsed_scores[1] != "-1" or parsed_scores[0] == "-1":
+        parsed_scores[1] = parsed_scores[1][1:]
+    return parsed_scores
+
+
 class Class(object):
     def __init__(self, body):
         self._body = body
@@ -56,6 +71,7 @@ class Class(object):
             temp = player
             if temp["id"] == team_id:
                 return temp["name"]
+        logging.error(f"Didn't find player {team_id} in challonge response.")
         return None
 
     @property
@@ -64,6 +80,9 @@ class Class(object):
 
     def replace_acronyms(self, bracket_data: bracket.Class) -> bracket.Class:
         ladder_data = ladder.Class(bracket_data)
+        if not self.body:
+            logging.fatal("Did not receive JSON response!")
+            exit(-1)
         for i in range(len(self.body["matches"])):
             challonge_match = self.body["matches"][i]
             try:
@@ -76,12 +95,12 @@ class Class(object):
                 match_id = get_match_id(challonge_match["identifier"])
                 bracket_match = ladder_data.get_match(match_id, True)
                 minus_pos = challonge_match["scores_csv"].find("-")
-                if minus_pos == 0:
-                    scores = [0, bracket_match.PointsToWin]
-                elif not challonge_match.get("scores_csv"):
+                if not challonge_match.get("scores_csv"):
                     scores = [0, 0]
                 else:
-                    scores = count_scores(challonge_match["scores_csv"])
+                    scores = calculate_scores(challonge_match["scores_csv"])
+                    logging.debug(f"Parsed {challonge_match["scores_csv"]} as "
+                                  f"Team1: {scores[0]}, Team2: {scores[1]}.")
                 bracket_match.replace_acronyms(acronyms)
                 bracket_match = assign_scores(bracket_match, scores)
                 bracket_match.Completed = True
